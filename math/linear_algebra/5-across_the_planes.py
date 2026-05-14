f8e9e43177eae7cf02506c8bc3bd6dd98c639e69
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-14 10:18:20 +0000

    across the pane

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+"""Across The Planes"""
+
 
 def add_matrices2D(mat1, mat2):
+    """ add matrix"""
     if len(mat1) != len(mat2):
         return None
-    
+
     for i in range(len(mat1)):
         if len(mat1[i]) != len(mat2[i]):
             return None
-    
+
     return [[mat1[i][j] + mat2[i][j]
              for j in range(len(mat1[i]))
              ] for i in range(len(mat1))]
