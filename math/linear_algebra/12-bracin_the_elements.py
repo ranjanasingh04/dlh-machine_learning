6f3ec241ba40e4e4af586425bf65e928d6821da4
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-15 10:33:46 +0000

    Elementwise calculation

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Bracing The Elements"""
+
+
+
+def np_elementwise(mat1, mat2):
+    """ Elementwise """
+    return (mat1 + mat2,
+            mat1 - mat2,
+            mat1 * mat2,
+            mat1 / mat2)
