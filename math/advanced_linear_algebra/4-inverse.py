6038f26331e14f757492a0b4e872e3ec2c401c03
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-27 10:05:49 +0000

    Inverse

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -96,6 +96,14 @@ def inverse(matrix):
 
     Returns None if matrix is singular.
     """
+    if (not isinstance(matrix, list) or
+            not all(isinstance(row, list) for row in matrix)):
+        raise TypeError("matrix must be a list of lists")
+
+    if (len(matrix) == 0 or matrix == [[]] or
+            any(len(row) != len(matrix) for row in matrix)):
+        raise ValueError("matrix must be a non-empty square matrix")
+
     det = determinant(matrix)
 
     if det == 0:
