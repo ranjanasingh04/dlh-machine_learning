cb6ef0511a478d6391b80bafed18473a95feb0ab
Ranjana Singh <ranjanasingh04@gmail.com>
2026-06-07 15:01:39 +0200

    Gradient

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module that plots mountain elevation data."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """Create a scatter plot of mountain elevations."""
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+
+    plt.figure(figsize=(6.4, 4.8))
+
+    scatter = plt.scatter(x, y, c=z, cmap='viridis')
+
+    plt.xlabel('x coordinate (m)')
+    plt.ylabel('y coordinate (m)')
+    plt.title('Mountain Elevation')
+
+    cbar = plt.colorbar(scatter)
+    cbar.set_label('elevation (m)')
+
+    plt.show()
