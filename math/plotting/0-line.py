39d5849c8b0697ae8521c0a3ca5ad1f881c48d0c
Ranjana Singh <ranjanasingh04@gmail.com>
2026-06-07 14:30:27 +0200

    Line Graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Module that plots y = x^3 as a red line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Plot y = x^3 as a solid red line."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, 'r-')
