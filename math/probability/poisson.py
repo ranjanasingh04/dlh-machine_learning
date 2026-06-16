7de69da9758aaba1c660d8a284311ad4d8f5cfc1
Ranjana Singh <ranjanasingh04@gmail.com>
2026-06-16 17:36:55 +0000

    Poisson Distribution

---
+++ math/probability/poisson.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Poisson distribution"""
+
+
+class Poisson:
+    def __init__(self, data=None, lambtha=1):
+        """Initialize Poisson distribution"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.data = data
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+
+        if len(data) < 2:
+            raise ValueError("data must contain multiple values")
+        self.lambtha = float((sum(data)/len(data)))
