3b0c5272f70ccf1d261763d5c963ffea05492fe9
Ranjana Singh <ranjanasingh04@gmail.com>
2026-07-07 20:45:47 +0000

    Dataframe, From Dictionary

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -6,7 +6,7 @@ Creates a pandas DataFrame from a dictionary.
 import pandas as pd
 
 data = {
-    "First" :[0.0, 0.5, 1.0, 1.5],
-    "Second" :["one", "two", "three", "four"]
+    "First": [0.0, 0.5, 1.0, 1.5],
+    "Second": ["one", "two", "three", "four"]
 }
 df = pd.DataFrame(data, index=["A", "B", "C", "D"])
