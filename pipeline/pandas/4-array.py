e7a07d8e2718ebf5f6cd0f9c542d617a7050feda
Ranjana Singh <ranjanasingh04@gmail.com>
2026-07-08 20:13:06 +0000

    From numby

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -3,10 +3,10 @@
 This module contains the function array.
 """
 
+
 def array(df):
     """
     Select the last 10 rows of the High and Close columns
     and return them as a NumPy array.
     """
     return df[["High", "Close"]].tail(10).to_numpy()
-    
