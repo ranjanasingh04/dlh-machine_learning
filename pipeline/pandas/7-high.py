56ff458a68690652e45e2eb522192d56bfb187e9
Ranjana Singh <ranjanasingh04@gmail.com>
2026-07-08 20:26:21 +0000

    Sort

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains the function high.
+"""
+
+def high(df):
+    """
+    Sort the DataFrame by the High column in descending order.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: Sorted DataFrame.
+    """
+    return df.sort_values(by="High", ascending=False)
